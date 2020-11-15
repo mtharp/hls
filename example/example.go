24f0b03a9df3ae369c729634d9750e0dd9c569b0
Michael Tharp <gxti@partiallystapled.com>
2020-11-15 15:27:41 -0500

    Add example server

---
+++ example/example.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"strings"
+	"time"
+
+	"eaglesong.dev/hls"
+	"github.com/nareix/joy4/av/avutil"
+	"github.com/nareix/joy4/format/rtmp"
+	"golang.org/x/sync/errgroup"
+)
+
+func main() {
+	pub := &hls.Publisher{}
+	rts := &rtmp.Server{
+		HandlePublish: func(c *rtmp.Conn) {
+			defer c.Close()
+			log.Println("publish started from", c.NetConn().RemoteAddr())
+			if err := avutil.CopyFile(pub, c); err != nil {
+				log.Printf("error: publishing from %s: %+v", c.NetConn().RemoteAddr(), err)
+			}
+		},
+	}
+	var eg errgroup.Group
+	eg.Go(rts.ListenAndServe)
+
+	http.Handle("/hls/", pub)
+	http.Handle("/", http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		r := strings.NewReader(home)
+		http.ServeContent(rw, req, "index.html", time.Time{}, r)
+	}))
+	eg.Go(func() error {
+		return http.ListenAndServe(":8080", nil)
+	})
+	log.Println("listening on rtmp://localhost/live and http://localhost:8080")
+	if err := eg.Wait(); err != nil {
+		log.Println("error:", err)
+	}
+}
+
+const home = `<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>HLS demo</title>
+<script src="https://cdn.jsdelivr.net/npm/hls.js@latest"></script>
+</head>
+<body>
+<video id="video" muted autoplay controls></video>
+<script>
+let hls = new Hls();
+hls.loadSource('/hls/index.m3u8');
+hls.attachMedia(document.getElementById('video'));
+// hls.on(Hls.Events.MANIFEST_PARSED, () => video.play());
+</script>
+</body>
+</html>
+`
--- go.mod
+++ go.mod
@@ -4,4 +4,7 @@ go 1.12
 
 replace github.com/nareix/joy4 => eaglesong.dev/joy4 v0.0.0-20190831160920-566887487cc0
 
-require github.com/nareix/joy4 v0.0.0-00010101000000-000000000000
+require (
+	github.com/nareix/joy4 v0.0.0-00010101000000-000000000000
+	golang.org/x/sync v0.0.0-20201020160332-67f06af15bc9
+)
